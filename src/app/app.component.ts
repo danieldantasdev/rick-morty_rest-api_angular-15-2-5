722555ba7e27c1f7c9b5b13e5814b573fe4e39ee
lobinhodev <contatolobinhodev@gmail.com>
2023-04-08 20:38:24 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rick-morty_rest-api_angular-15-2-5';
+}
